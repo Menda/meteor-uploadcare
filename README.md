dfa278eb3d621f7553ffcfa314f948e327915ac8
Samuel Goudie <hello@samuelgoudie.co.uk>
2015-12-03 14:57:56 +0000

    Initial package structure

---
+++ README.md
@@ -0,0 +1,5 @@
+#Small Joys: Meteor Uploadcare
+
+A little package that adds Uploadcare to Meteor, and gives you some methods for storage and removal.
+
+Note: This package is developed for the Small Joys workflow. If you require something a little lighter, we recommend <a href="https://github.com/nate-strauser/meteor-uploadcare-plus/">Nate Strauser's package</a>.
---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'smalljoys:uploadcare',
+  version: '0.0.1',
+  summary: 'Joyful Meteor and Uploadcare integration',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+
+  api.use('ecmascript');
+
+  api.addFiles(['client.js'],'client');
+  api.addFiles(['server.js'],'server');
+
+  api.export('loadUploadcare', 'client');
+});
