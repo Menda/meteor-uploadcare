7df4d93cb7926588d2492acfb2c4e0b7a9caef17
Samuel Goudie <hello@samuelgoudie.co.uk>
2015-12-07 15:34:45 +0000

    Opened Ucare method to server

--- package.js
+++ package.js
@@ -13,10 +13,10 @@ Package.onUse(function(api) {
   api.use(['underscore','check','http'], 'client');
   api.use(['browser-policy@1.0.5'], 'server', {weak: false, unordered: false});
 
-  api.addFiles(['ucare.js'],'client');
+  api.addFiles(['ucare.js'],['client','server']);
   api.addFiles(['methods.js'],'server');
   api.addFiles(['policy.js'],'server');
 
-  api.export('Ucare', 'client');
+  api.export('Ucare', ['client','server']);
   api.export(['addToMediaStorage', 'removeFromMediaStorage'], ['client','server']);
 });
