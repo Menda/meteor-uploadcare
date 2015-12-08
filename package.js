c91b3126541ecda2bb9df20976c7466149780be6
Samuel Goudie <hello@samuelgoudie.co.uk>
2015-12-08 10:07:14 +0000

    Updated version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'smalljoys:uploadcare',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: 'Joyful Meteor and Uploadcare integration',
   git: 'https://github.com/smalljoys/meteor-uploadcare',
   documentation: 'README.md'
